0cdcbc58ceb53cb2a37f8b76a3823dba8cc053c9
Morne Venter <morneventer@pop-os.localdomain>
2020-02-13 11:37:10 +0200

    init commit

---
+++ graph.py
@@ -0,0 +1,15 @@
+
+import matplotlib.pyplot as plt
+from radiospectra.sources.callisto import CallistoSpectrogram
+
+files = 'type3.fit.gz'
+image = CallistoSpectrogram.read(files)
+nobg = plt.figure(figsize=(16,6))
+nobg = image.subtract_bg()
+
+
+nobg.plot(vmin=12, vmax = 255, cmap='inferno')
+plt.axis('equal')
+plt.title ('Type 3')
+plt.savefig('output.png')
+plt.show()
