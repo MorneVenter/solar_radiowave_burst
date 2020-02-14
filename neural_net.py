8756a77207c1f1043e0f2a0bbd9e134e9f839962
Morne Venter <morneventer.mv@gmail.com>
2020-02-14 07:48:28 +0200

    more nets

--- neural_net.py
+++ neural_net.py
@@ -10,8 +10,6 @@ img_path = 'output.png'
 bgr_img = cv2.imread(img_path)
 gray_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2GRAY)
 gray_img = gray_img.astype("float32")/255
-plt.imshow(gray_img, cmap='gray')
-
 
 filter_vals = np.array([[-1, -1, 1, 1], [-1, -1, 1, 1], [-1, -1, 1, 1], [-1, -1, 1, 1]])
 filter_1 = filter_vals
@@ -29,7 +27,7 @@ class Net(nn.Module):
         k_height, k_width = weight.shape[2:]
         self.conv = nn.Conv2d(1, 4, kernel_size=(k_height, k_width), bias=False)
         self.conv.weight = torch.nn.Parameter(weight)
-def forward(self, x):
+    def forward(self, x):
         conv_x = self.conv(x)
         activated_x = F.relu(conv_x)
         return conv_x, activated_x
@@ -38,11 +36,32 @@ def forward(self, x):
 weight = torch.from_numpy(filters).unsqueeze(1).type(torch.FloatTensor)
 model = Net(weight)
 
-def viz_layer(layer, n_filters= 4):
-    fig = plt.figure(figsize=(20, 20))
+# def viz_layer(layer, n_filters = 4):
+#     fig = plt.figure(figsize=(20, 20))
+#     for i in range(n_filters):
+#         ax = fig.add_subplot(1, n_filters, i+1, xticks=[], yticks=[])
+#         ax.imshow(np.squeeze(layer[0,i].data.numpy()), cmap='gray')
+#         ax.set_title('Output %s' % str(i+1))
+#     plt.show()
 
-    for i in range(n_filters):
-        ax = fig.add_subplot(1, n_filters, i+1, xticks=[], yticks=[])
-        # Grab layer outputs
-        ax.imshow(np.squeeze(layer[0,i].data.numpy()), cmap='gray')
-        ax.set_title('Output %s' % str(i+1))
+# plt.imshow(gray_img, cmap='gray')
+# fig = plt.figure(figsize=(12, 6))
+# fig.subplots_adjust(left=0, right=1.5, bottom=0.8, top=1, hspace=0.05, wspace=0.05)
+# for i in range(4):
+#     ax = fig.add_subplot(1, 4, i+1, xticks=[], yticks=[])
+#     ax.imshow(filters[i], cmap='gray')
+#     ax.set_title('Filter %s' % str(i+1))
+
+gray_img_tensor = torch.from_numpy(gray_img).unsqueeze(0).unsqueeze(1)
+conv_layer, activated_layer = model(gray_img_tensor)
+
+gray = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2GRAY)
+# 3x3 array for edge detection
+sobel_y = np.array([[ -1, -2, -1],
+                   [ 0, 0, 0],
+                   [ 1, 2, 1]])
+sobel_x = np.array([[ -1, 0, 1],
+                   [ 0, 0, 0],
+                   [ 1, 2, 1]])
+
+filtered_image = cv2.filter2D(gray, -1, sobel_y)
